2a90fdb16b02a230f6f9f3b17314b8dea491d437
edgoadcom <ed@edgoad.com>
2024-11-22 12:05:40 -0800

    Added label

---
+++ end/CH10/tkinter_label.py
@@ -0,0 +1,30 @@
+# Second tkinter script
+# Add a button and command
+# Create by Ed 11/15
+
+# Import tkinter
+import tkinter
+from tkinter import messagebox
+
+# Functions
+def button_clicked():
+    tkinter.Label(my_window, text = "button was clicked").pack()
+    my_label.configure(text = "New text",
+                       font = ("Courier", 75),
+                       fg = "lightyellow",
+                       bg = "darkred" )
+    messagebox.showerror("Error", "Dont click that")
+
+# Create the GUI main window
+my_window = tkinter.Tk()
+
+# Add widgets
+my_label = tkinter.Label(my_window, text = "Hello World",
+                         font = ("Arial Bold", 50))
+my_label.pack()
+my_button = tkinter.Button(my_window, text = "Click Here",
+                           command = button_clicked)
+my_button.pack()
+
+# Enter the main event loop
+my_window.mainloop()
