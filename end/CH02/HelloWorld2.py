c91f10ab0fdc2360aa64ead23966f19373cd30d0
edgoadcom <ed@edgoad.com>
2025-02-26 13:21:28 -0800

    Added printing options

--- end/CH02/HelloWorld2.py
+++ end/CH02/HelloWorld2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # A simple "Hello World" script in python with Inputs
-# Created by Ed Goad, 2/3/2021
+# Created by Ed Goad, 2/3
 
 your_name = input("What is your name? ")
 print("Hello {0}".format(your_name))
--- end/CH02/HelloWorld3.py
+++ end/CH02/HelloWorld3.py
@@ -1,11 +1,30 @@
 #!/usr/bin/env python3
 # A simple "Hello World" script in python with Inputs
-# Created by Ed Goad, 2/3/2021
+# Created by Ed Goad, 2/3
 
+# Get users name
 your_name = input("What is your name? ")
+
+# String formatting and f-string
 print("Hello {0}".format(your_name))
 print(f"Hello {your_name}")
+
+# String concatenation
 print("Hello " + your_name)
+
+# Multiple print arguments
 print("Hello", your_name)
+
+# Constructing new variables
 message = "Hello" + " " + your_name
 print(message)
+
+# Removing newline
+print("Hello ", end = "")
+print(your_name)
+
+# Old style formatting
+print("Hello %s" % your_name)
+
+# Joining text
+print(" ".join(["Hello", your_name]))
