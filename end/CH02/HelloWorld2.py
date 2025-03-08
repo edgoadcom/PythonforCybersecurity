29c8446ea1345761261aa3dc143ccc0782faf0a2
Quentin Athula <q.athula@hotmail.com>
2025-03-08 18:44:06 -0500

    Commit Genny changes!

--- end/CH02/HelloWorld2.py
+++ end/CH02/HelloWorld2.py
@@ -3,9 +3,9 @@
 # Created by Quentin Athula, 3/8/2025
 
 your_name = input("What is your name? ")
-print("Hello {0}".format(your_name))
-print(f"Hello {your_name}")
-print("Hello " + your_name)
-print("Hello", your_name)
-message = "Hello" + " " + your_name
+print("Genny {0}".format(your_name))
+print(f"Genny {your_name}")
+print("Genny " + your_name)
+print("Genny", your_name)
+message = "Genny" + " " + your_name
 print(message)
