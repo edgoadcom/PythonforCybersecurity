972c9990bea07b54bff6171d6e9bb086afcbc313
Quentin Athula <q.athula@hotmail.com>
2025-03-08 18:37:47 -0500

    Commit this two changes!

--- end/CH02/HelloWorld.py
+++ end/CH02/HelloWorld.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
 # A simple "Hello World" script in python
-# Created by Ed Goad, 2/3/2021
-print("Hello World")
+# Created by Quentin Athula, 3/8/2025
+
+your_name = input("what is your name? ")
+print("Gwy {0}".format(your_name))
