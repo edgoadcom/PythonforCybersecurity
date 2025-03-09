7773b06ed133b87f49061bad4df81ab7b3e4d4b1
Quentin Athula <q.athula@hotmail.com>
2025-03-09 14:08:08 -0400

    Commit simplecalculatoy.py

--- end/CH02/SimpleCalculator.py
+++ end/CH02/SimpleCalculator.py
@@ -1,14 +1,14 @@
-#!/usr/bin/env python3
-# A simple calculator to show math and conditionals
-# Created by Ed Goad, 2/3/2021
+#!/user/bin/env python3
+# A simple calculator script in python
+# Created by Quentin Athula, 3/9/2025
 
-# Get inputs first
-# Note we are casting the numbers as "float", we could also do "int"
+# Get input from the user
+# Note we are casting the numbers as "float", we could also cast them as "int"
 first_num = float(input("What is the first number: "))
 activity = input("What activity? ( + - * / ) ")
 second_num = float(input("What is the second number: "))
 
-# depending on the selected activity, perform an action
+# depending on the selected activity, perform the calculation
 if activity == "+":
     print(first_num + second_num)
 if activity == "-":
@@ -17,3 +17,4 @@ if activity == "*":
     print(first_num * second_num)
 if activity == "/":
     print(first_num / second_num)
+
