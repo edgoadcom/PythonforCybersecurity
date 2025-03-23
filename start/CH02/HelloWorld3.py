0d5acd77b0f4b1df06eead853c8ec11787da90ba
Quentin Athula <q.athula@hotmail.com>
2025-03-22 22:56:05 -0400

    commit message what is your name2?

--- start/CH02/HelloWorld3.py
+++ start/CH02/HelloWorld3.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
 # A more complex "Hello World" script in python with Inputs
-# Created 
+# Created by Quentin Athula 3/22/2025
+
+your_name = input("What is your name? ")
+print("Hello {0} ".format(your_name))
+print(f"Hello {your_name}")
+print("Hello" + your_name)
+print("Hello", your_name)
+message = "Hello" + " " + your_name
+print(message)
