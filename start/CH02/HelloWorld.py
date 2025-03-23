651b20a7b572da1035ad7eefc024fe0bd7bed70d
Quentin Athula <q.athula@hotmail.com>
2025-03-22 20:04:28 -0400

    This is Commit conformed that IDLE works!

--- start/CH02/HelloWorld.py
+++ start/CH02/HelloWorld.py
@@ -1,3 +1,4 @@
 #!/usr/bin/env python3
-# A simple "Hello World" script in python
-# Created by Quentin Athula 7/8/2024
+# A simple "check that the IDLE works" script in python
+# Created by Quentin Athula 3/22/2025
+print("This is a sample to check that the IDLE works")
