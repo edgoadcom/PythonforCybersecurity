120963cfb26048f8acf2fb6c7ac14c3f001fd8fe
Quentin Athula <q.athula@hotmail.com>
2025-03-22 21:07:37 -0400

    This is a sample to IDLE

---
+++ start/CH02/HelloWorld1.py
@@ -0,0 +1,4 @@
+#!/user/bin/.venv/ python3
+# A sample "Check that the IDLE is working" script in python
+# Created by Quentin Athula 3/22/2025
+print("This is a sample to check that the IDE is working")
