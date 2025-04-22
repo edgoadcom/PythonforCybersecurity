2a02cbcd06333670fee4567e9559395d4e26141e
Quentin Athula <q.athula@hotmail.com>
2025-04-22 12:11:20 -0400

    First example of pinging from python!

--- start/CH03/pinger1.py
+++ start/CH03/pinger1.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 # First example of pinging from Python
-# By 
+# By Quentin Athula
+# 4/22/2025
+
+# import necessary python modules
+import platform
+import os
+
+# Assign IP address to ping to a variable
+ip = "127.0.0.1"
+# Build the command to ping the IP address
+ping_cmd = f"ping -c 1 -w 2 {ip} > /dev/null 2>&1"
+# Execute command and capture exit code
+exit_code = os.system(ping_cmd)
+# print the result to the console
+print(exit_code)
